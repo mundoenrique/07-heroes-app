432eae8efc13f9e2e6bf5f1800ee6807f2dc79d0
mundoenrique <mundoenrique@hotmail.com>
2022-03-22 14:12:32 -0400

    Separación de rutas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
-import DcScreen from '../components/dc/DcScreen';
-import NavBar from '../components/iu/NavBar';
 import LoginScree from '../components/login/LoginScree';
-import MarvelScreen from '../components/marvel/MarvelScreen';
-import SearchScreen from '../components/search/SearchScreen';
+import DashboardRouter from './DashboardRouter';
 
 export default function AppRouter() {
 	return (
 		<>
-			<NavBar />
 			<Routes>
-				<Route path="/" element={<MarvelScreen />} />
-				<Route path="/marvel" element={<MarvelScreen />} />
-				<Route path="/dc" element={<DcScreen />} />
-				<Route path="/search" element={<SearchScreen />} />
 				<Route path="/login" element={<LoginScree />} />
+				<Route path="/*" element={<DashboardRouter />} />
 			</Routes>
 		</>
 	);
---
+++ src/routers/DashboardRouter.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+
+import DcScreen from '../components/dc/DcScreen';
+import NavBar from '../components/iu/NavBar';
+import MarvelScreen from '../components/marvel/MarvelScreen';
+import SearchScreen from '../components/search/SearchScreen';
+
+export default function DashboardRouter() {
+	return (
+		<>
+			<NavBar />
+			<Routes>
+				<Route path="marvel" element={<MarvelScreen />} />
+				<Route path="dc" element={<DcScreen />} />
+				<Route path="search" element={<SearchScreen />} />
+				<Route path="/" element={<MarvelScreen />} />
+			</Routes>
+		</>
+	);
+}
