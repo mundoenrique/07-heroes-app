0dd52acccb60cd9c87f960c5297cc2a5fa310a57
mundoenrique <mundoenrique@hotmail.com>
2022-03-23 15:11:41 -0400

    Agrega paquetes y configuraciones para pruebas

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
+// Para realizar pruebas es necesario hacer la instación de los siguientes paquetes
+// como dependencias de desarrollo
+// "enzyme @wojtekmaj/enzyme-adapter-react-17 enzyme-to-json @testing-library/react-hooks"
 import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { createSerializer } from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
